80ae37d18cd4466dfca01f313b5ec9c45f6ca67c
Carl Joe <inocentescarljoe12@gmail.com>
2025-09-18 21:32:49 +0800

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const userController = require("../controllers/user");
+
+const router = express.Router();
+const auth = require("../auth");
+
+const {verify} = auth;
+
+router.post("/register", userController.registerUser);
+router.post("/login", userController.loginUser);
+router.get("/details", verify, userController.getProfile);
+
+module.exports = router;
